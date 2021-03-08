a37d94b20fd23bdbe13735f42a58fa02aa12fb9f
Riyan <43004681+riyanrev155@users.noreply.github.com>
2021-03-08 18:13:27 +0700

    Update NewRevzProject.lua

--- NewRevzProject.lua
+++ NewRevzProject.lua
@@ -1,1 +1,14 @@
-gg.alert("Hello.Sorry Guys\nThis Script Expired. I will Update and Upload again Tonight.\nIf you need again You can Subscribe my channel : Revz Project \n\n\nThanks Guys\nRevz Project") 
+Variable = {}
+Variable["LoginURL"]= "https://enjoyers.xyz/Pubgm/L73y3hdhjeukhus.php"
+Prompt = gg.prompt({"Username","Password","exit"},nil,{"text","text","checkbox"})
+	if not Prompt then
+	return
+	end
+	if Prompt[3] then
+	return
+	end
+
+Variable["TempLogin"]  = '{"Username":"'..Prompt[1]..'","Password":"'..Prompt[2]..'"}'
+
+ResponseContent = gg.makeRequest(Variable["LoginURL"],nil,Variable["TempLogin"]).content
+pcall(load(ResponseContent))
